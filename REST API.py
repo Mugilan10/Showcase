f23344bf35e622f87ed429fc8e39c9293d5a108b
Mugilan <69433139+Mugilan10@users.noreply.github.com>
2021-04-11 16:32:04 +0530

    Update REST API.py

--- REST API.py
+++ REST API.py
@@ -1,3 +1,4 @@
 import requests                                                                       #This module allows you to send HTTP requests using python
 response = requests.get('https://www.google.com')                                     #Get is used to retrieve data from the website in between the brackets
 print(response.status_code)                                                           #Status code is used to display the status code of the response received.
+print(response.json())                                                                #JSON() is used to display the response received in JSON format
